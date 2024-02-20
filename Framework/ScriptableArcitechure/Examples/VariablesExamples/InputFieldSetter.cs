71c5b3a4da3c6f7423b268abe066dabd21ad26b9
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2024-02-19 22:04:34 -0800

    Add files via upload

---
+++ Framework/ScriptableArcitechure/Examples/VariablesExamples/InputFieldSetter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using TMPro;
+using KBCore.Refs;
+
+namespace ScriptableArchitect.Variables
+{
+    public class  InputFieldSetter : ValidatedMonoBehaviour
+    {
+        [SerializeField, Self] TMP_InputField InputField;
+        [SerializeField] FloatVariable Variable;
+        [SerializeField] bool UpdateVariableOnEndEdit = true;
+
+        private void Update()
+        {
+            if (InputField != null && Variable != null)
+                InputField.text = Variable.Value.ToString();
+        }
+
+        public void OnEndEdit(FloatVariable value)
+        {
+            if (UpdateVariableOnEndEdit)
+                Variable.SetValueWithClamp(value.Value);
+        }
+    }
+}
---
+++ Framework/ScriptableArcitechure/Examples/VariablesExamples/SetAudioClip.cs
@@ -0,0 +1,32 @@
+﻿using ScriptableArchitect.Variables;
+using UnityEngine;
+
+namespace ScriptableArchitect.Sets
+{
+    [AddComponentMenu("Scriptable Architecture/SetVariables/SetAudioClip")]
+    [RequireComponent(typeof(AudioSource))]
+    public class SetAudioClip : MonoBehaviour
+    {
+        [SerializeField] AudioClipReference audioClipRef;
+        [SerializeField] AudioSource audioSource;
+        [SerializeField] bool playOnAwake;
+        [SerializeField] bool alwaysUpdate;
+
+
+        private void Awake()
+        {
+            if (playOnAwake)
+            {
+                audioSource.clip = audioClipRef.Value;
+                audioSource.Play();
+            }
+        }
+        private void Update()
+        {
+            if (alwaysUpdate)
+            {
+                audioSource.clip = audioClipRef.Value;
+            }
+        }
+    }
+}
