cb8be37ad3300589b80e012b33a45896b14894b0
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2024-02-21 00:08:20 -0800

    Add files via upload

---
+++ Runtime/ScriptableArcitechure/ScriptableArcitechure/_Core/Events/FloatEventChannel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableArchitect.Events
+{
+    /// <summary>
+    /// Class for an event channel that can handle float events.
+    /// </summary>
+    [CreateAssetMenu(menuName = "ScriptableArchitect/Events/FloatEventChannel")]
+    public class FloatEventChannel : EventChannel<float> { }
+}
---
+++ Runtime/ScriptableArcitechure/ScriptableArcitechure/_Core/Events/IntEventChannel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ScriptableArchitect.Events
+{
+    /// <summary>
+    /// Class for an event channel that can handle integer events.
+    /// </summary>
+    [CreateAssetMenu(menuName = "ScriptableArchitect/Events/IntEventChannel")]
+    public class IntEventChannel : EventChannel<int> { }
+}
