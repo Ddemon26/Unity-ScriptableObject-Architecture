18a1d21dec9d3783392e6fc489a2a258583b76dc
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2024-02-21 00:07:59 -0800

    Delete Runtime/ScriptableArcitechure/ScriptableArcitechure/_Core/Events directory

--- Runtime/ScriptableArcitechure/ScriptableArcitechure/_Core/Events/FloatEventListener.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ScriptableArchitect.Events
-{
-    /// <summary>
-    /// A class that listens for events of type float.
-    /// </summary>
-    public class FloatEventListener : EventListener<float> { }
-}
