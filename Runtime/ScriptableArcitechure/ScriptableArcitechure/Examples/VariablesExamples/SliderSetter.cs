eb3e39acace7bef8c7422e7e60bc8f68d23120f9
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2024-02-21 22:18:03 -0800

    Update SliderSetter.cs

--- Runtime/ScriptableArcitechure/ScriptableArcitechure/Examples/VariablesExamples/SliderSetter.cs
+++ Runtime/ScriptableArcitechure/ScriptableArcitechure/Examples/VariablesExamples/SliderSetter.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace ScriptableArchitect.Variables
@@ -21,6 +21,31 @@ namespace ScriptableArchitect.Variables
         [SerializeField] FloatVariable Variable;
 
         /// <summary>
+        /// Validates the Slider component.
+        /// </summary>
+        private void OnValidate()
+        {
+            if (Slider == null)
+                Slider = GetComponent<Slider>();
+        }
+
+        /// <summary>
+        /// Adds a listener to the Slider's onValueChanged event.
+        /// </summary>
+        private void Awake()
+        {
+            Slider.onValueChanged.AddListener(OnValueChange);
+        }
+
+        /// <summary>
+        /// Removes the listener from the Slider's onValueChanged event.
+        /// </summary>
+        private void OnDisable()
+        {
+            Slider.onValueChanged.RemoveListener(OnValueChange);
+        }
+
+        /// <summary>
         /// Update is called every frame, if the MonoBehaviour is enabled.
         /// It sets the value of the Slider to the value of the FloatVariable.
         /// </summary>
@@ -28,6 +53,15 @@ namespace ScriptableArchitect.Variables
         {
             if (Slider != null && Variable != null)
                 Slider.value = Variable.Value;
+        }
+
+        /// <summary>
+        /// Sets the value of the FloatVariable to the value of the Slider.
+        /// </summary>
+        /// <param name="value">The new value of the Slider.</param>
+        public void OnValueChange(float value)
+        {
+            Variable.SetValueWithClamp(value);
         }
     }
 }
