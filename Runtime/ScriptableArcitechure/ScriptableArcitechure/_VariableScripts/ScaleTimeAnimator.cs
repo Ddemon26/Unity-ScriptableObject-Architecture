b99afbbbd284c19c74e79897b132cad4458690e6
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2024-03-17 20:31:02 -0700

    removed Input Reader System
    yup

---
+++ Runtime/ScriptableArcitechure/ScriptableArcitechure/_VariableScripts/ScaleTimeAnimator.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace ScriptableArchitect.Variables
+{
+    /// <summary>
+    /// Controls the speed of an Animator based on a Boolean variable.
+    /// </summary>
+    [AddComponentMenu("Scriptable Architect/Variables/AnimatorSetters/ScaleTimeAnimator", 1)]
+    [HelpURL("https://www.youtube.com/watch?v=raQ3iHhE_Kk&t=2132s")]
+    [RequireComponent(typeof(Animator))]
+    public class ScaleTimeAnimator : MonoBehaviour
+    {
+        /// <summary>
+        /// The Animator whose speed is to be controlled.
+        /// </summary>
+        [Tooltip("The Animator whose speed is to be controlled.")]
+        [SerializeField] Animator Animator;
+
+        /// <summary>
+        /// The Boolean variable that determines whether the Animator should be stopped or not.
+        /// </summary>
+        [Tooltip("The Boolean variable that determines whether the Animator should be stopped or not.")]
+        [SerializeField] BoolVariable stopAnimator;
+
+        /// <summary>
+        /// If set to true, the Animator will be stopped when stopAnimator is false and vice versa.
+        /// </summary>
+        [Tooltip("If set to true, the Animator will be stopped when stopAnimator is false and vice versa.")]
+        [SerializeField] bool invertSetting;
+
+        /// <summary>
+        /// Validates the Animator component.
+        /// </summary>
+        void OnValidate()
+        {
+            if (Animator == null)
+                Animator = GetComponent<Animator>();
+        }
+        /// <summary>
+        /// Updates the Animator speed based on the stopAnimator value and invertSetting.
+        /// </summary>
+        private void Update()
+        {
+            Animator.speed = (stopAnimator.Value ^ invertSetting) ? 1 : 0;
+        }
+    }
+}
---
+++ Runtime/ScriptableArcitechure/ScriptableArcitechure/_VariableScripts/TextReplacer.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace ScriptableArchitect.Variables
+{
+    /// <summary>
+    /// This class is used to replace the text of a TextMeshProUGUI component with the value of a StringVariable.
+    /// </summary>
+    [AddComponentMenu("Scriptable Architect/Variables/UISetters/Text Replacer")]
+    [HelpURL("https://www.youtube.com/watch?v=raQ3iHhE_Kk&t=2132s")]
+    public class TextReplacer : MonoBehaviour
+    {
+        /// <summary>
+        /// The TextMeshProUGUI component whose text will be replaced.
+        /// </summary>
+        [Tooltip("The TextMeshProUGUI component whose text will be replaced.")]
+        public TMP_Text TextComponent;
+
+        /// <summary>
+        /// The StringVariable whose value will be used to replace the text.
+        /// </summary>
+        [Tooltip("The StringVariable whose value will be used to replace the text.")]
+        public StringVariable Variable;
+
+        /// <summary>
+        /// If set to true, the text will be updated every frame.
+        /// </summary>
+        [Tooltip("If set to true, the text will be updated every frame.")]
+        public bool AlwaysUpdate;
+
+        private string lastValue = "";
+
+        /// <summary>
+        /// Validates the TextMeshProUGUI component. Editor only.
+        /// </summary>
+        private void OnValidate()
+        {
+            if (TextComponent == null)
+                TextComponent = GetComponent<TMP_Text>();
+        }
+
+        /// <summary>
+        /// OnEnable is called when the object becomes enabled and active.
+        /// It updates the text of the TextMeshProUGUI component.
+        /// </summary>
+        private void OnEnable()
+        {
+            UpdateText();
+        }
+
+        /// <summary>
+        /// Awake is called when the script instance is being loaded.
+        /// We update the text of the TextMeshProUGUI component.
+        /// </summary>
+        private void Awake()
+        {
+            UpdateText();
+        }
+
+        /// <summary>
+        /// Update is called every frame, if the MonoBehaviour is enabled.
+        /// If AlwaysUpdate is true, it updates the text of the TextMeshProUGUI component.
+        /// </summary>
+        private void Update()
+        {
+            if (AlwaysUpdate && Variable.Value != lastValue)
+            {
+                UpdateText();
+            }
+        }
+
+        /// <summary>
+        /// Updates the text of the TextMeshProUGUI component to the value of the StringVariable.
+        /// </summary>
+        private void UpdateText()
+        {
+            if (Variable != null && TextComponent != null)
+            {
+                TextComponent.text = Variable.Value;
+                lastValue = Variable.Value;
+            }
+        }
+    }
+}
